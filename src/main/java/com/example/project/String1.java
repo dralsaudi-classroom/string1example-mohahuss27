ddf5e7b3462a75b7ae885107c79982497c88f9f7
mohahuss27 <mohahuss27@gmail.com>
2024-09-04 14:21:48 +0300

    Update String1.java

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -30,9 +30,20 @@ public class String1 {
             append(s.getChar(i));
     }
     public boolean equal(String1 s) {
-        throw new UnsupportedOperationException("equal() function is not supported yet.");
+        if(s.length !- size)
+		return false;
+	for(int i = 0; i < s.length(); i++){
+		if(s.charAt(i) != str[i])
+			return false;
+	}
+	return true;
     }
     public void reverse() {
-        throw new UnsupportedOperationException("reverse() function is not supported yet.");
+        char temp;
+	for (int i = 0; i < size / 2; i++){
+		temp = str[i];
+		str[i] = str[size - i];
+		str[size - 1] = temp;
+	}
     }
 }
